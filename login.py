d916ea93ed1e81ffa484db9d9bcf95e103b81d23
rezazamoh <rezazamoh@gmail.com>
2023-07-02 22:28:03 +0330

    +fixed categories bugs +added a hole login system with sql data base +added a logout menu at top screen

---
+++ login.py
@@ -0,0 +1,58 @@
+import sqlite3
+# Sqlite database and table
+def sqllitefile():
+    global conn
+    conn = sqlite3.connect('login_data.db')
+    global c
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS login_details
+                (USER_ID TEXT PRIMARY KEY NOT NULL,
+                 PASSWORD TEXT NOT NULL);''')
+
+def isEnglish(s):
+    try:
+        s.encode(encoding='utf-8').decode('ascii')
+    except UnicodeDecodeError:
+        return False
+    else:
+        return True
+        
+def validity(user):
+    if len(user) < 3 or len(user) > 16:
+        return 0
+    else:
+        if isEnglish(user):
+            return 1
+        else:
+            return 0
+        
+def register(user_id,password):
+    sqllitefile()
+    # If user_name already exists
+    if conn.execute("select USER_ID from login_details where USER_ID like ?", (user_id,)).fetchone():
+        return 'Duplicated Username!'
+    else:
+        if validity(user_id) and validity(password):
+            c.execute("INSERT INTO login_details (USER_ID, PASSWORD) \
+                VALUES (?, ?)", (user_id, password))
+            conn.commit()
+            c.close()
+            conn.close()
+            return '1'
+        else:
+            return 'Not a Valid Username or Pass'
+    
+def login(user_id,password):
+    sqllitefile()
+    #Check user
+    user_id_exists = c.execute("select USER_ID from login_details where USER_ID like ?", (user_id,)).fetchone()
+    if not user_id_exists:
+        return 'No such username!'
+    else:
+        #Chcek pass
+        password_id_exists = c.execute("select password from login_details \
+        where user_id = ? and password = ?", (user_id, password,)).fetchone()
+        if password_id_exists:
+            return '0'
+        else:
+            return 'Password is not correct!'       
--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 import sys
 import resorses.res
 import digikala
+import login
 from PyQt5.uic import loadUi
 from PyQt5.QtCore import QThread
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QMainWindow, QApplication , QListWidgetItem
+from PyQt5.QtWidgets import QMainWindow, QApplication , QListWidgetItem ,QMenu
 searched_phrase = ''
 result_list = []
+account = 'حساب کاربری'
 
 class main_screen(QMainWindow):
     def __init__(self):
         super(main_screen,self).__init__()
         loadUi("main.ui",self)
         self.search_button.clicked.connect(self.search_func)
+        self.login_button.clicked.connect(self.login_func)
         self.laptop_label.mousePressEvent = self.goto_lap
+        self.mobile_label.mousePressEvent = self.goto_mob
+        self.cooking_label.mousePressEvent = self.goto_cook
+        self.clothing_label.mousePressEvent = self.goto_cloth
+        self.digital_label.mousePressEvent = self.goto_digi
+        self.logout.triggered.connect(self.logout_func)
 
     def search_func(self):
         if len(self.search_input.text())>3:
@@ -27,10 +35,27 @@ class main_screen(QMainWindow):
         else: 
             self.error_dialog = QtWidgets.QErrorMessage()
             self.error_dialog.showMessage('لطفا حداقل ۴ حرف وارد کنید!')
+    
+    def login_func(self):
+        login_window = login_screen()
+        widget.addWidget(login_window)
+        widget.setCurrentIndex(widget.currentIndex()+1)
 
+    def logout_func(self):
+        global account
+        if account != 'حساب کابری':
+            account = 'حساب کابری'
+            self.account_menu.setTitle(account)
+            self.logged_out = QtWidgets.QErrorMessage()
+            self.logged_out.showMessage('از حساب خود با موفقیت خارج شدید')
+        else:
+            self.notlogged = QtWidgets.QErrorMessage()
+            self.notlogged.showMessage('داخل حسابی نیستید که بخواهید از آن خارج شوید!')
+
+    
     def goto_lap(self,event):
             global result_list
-            result_list = digikala.scan('گوشی موبایل')
+            result_list = digikala.scan('لپ تاپ')
             result_window = result_screen()
             widget.addWidget(result_window)
             widget.setCurrentIndex(widget.currentIndex()+1)           
@@ -44,25 +69,46 @@ class main_screen(QMainWindow):
 
     def goto_cook(self,event):
             global result_list
-            result_list = digikala.scan('گوشی موبایل')
+            result_list = digikala.scan('لوازم آشپرخانه')
             result_window = result_screen()
             widget.addWidget(result_window)
             widget.setCurrentIndex(widget.currentIndex()+1)
 
     def goto_cloth(self,event):
             global result_list
-            result_list = digikala.scan('گوشی موبایل')
+            result_list = digikala.scan('لباس و پوشاک')
             result_window = result_screen()
             widget.addWidget(result_window)
             widget.setCurrentIndex(widget.currentIndex()+1)
 
     def goto_digi(self,event):
             global result_list
-            result_list = digikala.scan('گوشی موبایل')
+            result_list = digikala.scan('لوازم دیجیتال')
             result_window = result_screen()
             widget.addWidget(result_window)
             widget.setCurrentIndex(widget.currentIndex()+1)
 
+class login_screen(QMainWindow):
+    def __init__(self):
+        super(login_screen,self).__init__()
+        loadUi("login.ui",self)
+        self.register_rb.setChecked(True)
+        self.done_button.clicked.connect(self.log_reg)
+
+    def log_reg(self):
+        if self.register_rb.isChecked():
+            respond = login.register(self.user_input.text(),self.pass_input.text())
+        if self.login_rb.isChecked():
+            respond = login.login(self.user_input.text(),self.pass_input.text())
+        if respond.isdigit():
+            global account
+            account = self.user_input.text()
+            widget.setCurrentIndex(widget.currentIndex()-1)
+            widget.removeWidget(self)
+            main_window.account_menu.setTitle(account)
+        else:
+            self.error_label.setText(respond)
+            
 class result_screen(QMainWindow):
     def __init__(self):
         super(result_screen,self).__init__()
